e3cc3ede9b031eebff94f3c1ab050e4850e2b462
Ihor92 <salfetnikov92@gmail.com>
2019-07-14 18:09:30 +0300

    Divided dishes component

---
+++ src/Components/Dish/Dish.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Dish.css";
+import { restaurants } from "../InfoCard.js";
+
+export function Dish({ section }) {
+  return section.itemUuids.map((item, i) => {
+    let dishPhoto = restaurants.items[item].imageUrl;
+    return (
+      <>
+        <li className="dishes-item dishes-item__wrap" key={i}>
+          <div className="dishes-item__content">
+            <h5 className="dishes-menu__subtitle">
+              {restaurants.items[item].title}
+            </h5>
+            <span className="dishes-description">
+              {restaurants.items[item].itemDescription}
+            </span>
+            <span className="dishes-price">
+              {restaurants.items[item].price / 100 + " ₴"}
+            </span>
+          </div>
+
+          {dishPhoto && (
+            <div className="dishes-block__img">
+              <img
+                src={restaurants.items[item].imageUrl}
+                alt=""
+                className="dishes-img"
+              />
+            </div>
+          )}
+        </li>
+      </>
+    );
+  });
+}
--- src/Components/Dishes/Dishes.jsx
+++ src/Components/Dishes/Dishes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Dishes.css";
 import { restaurants } from "../InfoCard.js";
+import { Dish } from "../Dish/Dish";
 
 export function Dishes(props) {
   return (
@@ -8,38 +9,9 @@ export function Dishes(props) {
       {restaurants.sections.map((section, i) => {
         return (
           <div className="dishes dishes-wrap">
-            <h4 className="dishes-title">{section.title}</h4>
-            <ul className="dishes-list">
-              {section.itemUuids.map((item, i) => {
-                let dishPhoto = restaurants.items[item].imageUrl;
-                return (
-                  <>
-                    <li className="dishes-item dishes-item__wrap" key={i}>
-                      <div className="dishes-item__content">
-                        <h5 className="dishes-menu__subtitle">
-                          {restaurants.items[item].title}
-                        </h5>
-                        <span className="dishes-description">
-                          {restaurants.items[item].itemDescription}
-                        </span>
-                        <span className="dishes-price">
-                          {restaurants.items[item].price / 100 + " ₴"}
-                        </span>
-                      </div>
-
-                      {dishPhoto && (
-                        <div className="dishes-block__img">
-                          <img
-                            src={restaurants.items[item].imageUrl}
-                            alt=""
-                            className="dishes-img"
-                          />
-                        </div>
-                      )}
-                    </li>
-                  </>
-                );
-              })}
+            <h4 className="dishes__title">{section.title}</h4>
+            <ul className="dishes__list">
+              <Dish section={section} />
             </ul>
           </div>
         );
