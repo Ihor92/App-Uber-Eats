d32261371d666e6df17791253200effe8d207ac6
Ihor92 <salfetnikov92@gmail.com>
2019-07-14 20:46:59 +0300

    Changed class name to BEM #13

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,26 +4,24 @@ import { Link } from "react-router-dom";
 
 export function Card(props) {
   return (
-    <li className="restaurant__card">
+    <li className="card">
       <Link to="/restaurant">
         <img
-          className="restaurant__img"
+          className="card__img"
           src={props.imageUrl}
           alt="Food in restaurant"
         />
       </Link>
 
-      <span className="restaurant__name restaurant__name-uppercaset">
-        {props.title}
-      </span>
-      <span className="restaurant-kitchen">
-        {props.priceBucket} • {" "}
+      <span className="card__name card__name-uppercaset">{props.title}</span>
+      <span className="card__kitchen">
+        {props.priceBucket} •{" "}
         {props.categories.map(category => {
           return category.keyName + " • ";
         })}
       </span>
 
-      <span className="restaurant-delivery">
+      <span className="card__delivery">
         {props.etaRange.min} - {props.etaRange.max} Min
       </span>
     </li>
